a9778208fc2237ca5a4697eda26d6212010ea9c9
Bulat Bogdanov <bulatmaster73@gmail.com>
2025-04-05 10:09:45 +0500

    fixed init.py

--- init.py
+++ init.py
@@ -9,6 +9,8 @@ if os.path.exists(config.db_path):
     print('Бэкап сделан')
     os.remove(config.db_path)
 
+os.makedirs('data', exist_ok=True)
+
 with sqlite3.connect(config.db_path) as conn:
     with open('schema.sql') as f:
         conn.executescript(f.read())
